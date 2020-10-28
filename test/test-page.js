de8565342d4f18633f2ae51571897365d9ec7e16
Padraic Edwards <padraic.edwards@ie.ibm.com>
2020-10-28 11:52:55 +0000

    Delete test-page.js

--- test/test-page.js
+++
@@ -1,9 +0,0 @@
-var expect  = require('chai').expect;
-var request = require('request');
-
-it('Main page content', function(done) {
-    request('http://localhost:80' , function(error, response, body) {
-        expect(body).to.equal('Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service');
-        done();
-    });
-});
